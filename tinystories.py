9523d8f7ff1d1dc905abe8b855e9326e3c06780c
Matvey Kairov <mvkairov@edu.hse.ru>
2023-11-20 03:31:19 +0300

    pissitial

--- tinystories.py
+++ tinystories.py
@@ -25,42 +25,30 @@ class TinyStoriesDataset(Dataset):
     def __init__(self, data_file: str, sp_model_prefix: str = None,
                  vocab_size: int = 15000, normalization_rule_name: str = 'nmt_nfkc_cf',
                  model_type: str = 'bpe', max_length: int = 384):
-        """
-        Dataset with texts, supporting BPE tokenizer
-        :param data_file: txt file containing texts
-        :param train: whether to use train or validation split
-        :param sp_model_prefix: path prefix to save tokenizer model
-        :param vocab_size: sentencepiece tokenizer vocabulary size
-        :param normalization_rule_name: sentencepiece tokenizer normalization rule
-        :param model_type: sentencepiece tokenizer model type
-        :param max_length: maximal length of text in tokens
-        """
         if not os.path.isfile(sp_model_prefix + '.model'):
             # train tokenizer if not trained yet
             print("Starting tokenizer train...")
             SentencePieceTrainer.train(
                 input=data_file, vocab_size=vocab_size,
                 model_type=model_type, model_prefix=sp_model_prefix,
-                normalization_rule_name=normalization_rule_name,
-                pad_id=5,
+                normalization_rule_name=normalization_rule_name, pad_id=5,
             )
             print("Finished tokenizer train")
         # load tokenizer from file
 
-        print("Loading tokenizer...")
+        print("Tokenizer?")
         self.sp_model = SentencePieceProcessor(model_file=sp_model_prefix + '.model', )
-        print("Tokenizer loaded")
+        print("Tokenized.")
 
-        print("Loading texts...")
+        print("Texts?")
         with open(data_file, encoding="utf-8") as file:
             texts = list(map(lambda x: x.strip(), file.readlines()))
-
         self.texts = texts
-        print("Texts loaded")
+        print("Texted")
 
-        print("Encoding texts...")
+        print("Encoding?")
         self.indices = self.sp_model.encode(self.texts)
-        print("Texts encoded")
+        print("Encoded.")
 
         self.pad_id, self.unk_id, self.bos_id, self.eos_id = \
             self.sp_model.pad_id(), self.sp_model.unk_id(), \
@@ -69,43 +57,19 @@ class TinyStoriesDataset(Dataset):
         self.vocab_size = self.sp_model.vocab_size()
 
     def text2ids(self, texts: Union[str, List[str]]) -> Union[List[int], List[List[int]]]:
-        """
-        Encode a text or list of texts as tokenized indices
-        :param texts: text or list of texts to tokenize
-        :return: encoded indices
-        """
         return self.sp_model.encode(texts)
 
     def ids2text(self, ids: Union[torch.Tensor, List[int], List[List[int]]]) -> Union[str, List[str]]:
-        """
-        Decode indices as a text or list of tokens
-        :param ids: 1D or 2D list (or torch.Tensor) of indices to decode
-        :return: decoded texts
-        """
         if torch.is_tensor(ids):
             assert len(ids.shape) <= 2, 'Expected tensor of shape (length, ) or (batch_size, length)'
             ids = ids.cpu().tolist()
-
         return self.sp_model.decode(ids)
 
     def __len__(self):
-        """
-        Size of the dataset
-        :return: number of texts in the dataset
-        """
         return len(self.indices)
 
     def __getitem__(self, item: int) -> Tuple[torch.Tensor, int]:
-        """
-        Add specials to the index array and pad to maximal length
-        :param item: text id
-        :return: encoded text indices and its actual length (including BOS and EOS specials)
-        """
-        # These are placeholders, you may remove them.
-        indices = [self.bos_id] + self.indices[item][:self.max_length - 2] + [self.eos_id]
-        length = len(indices)
-        pad = torch.full((self.max_length,), self.pad_id, dtype=torch.int64)
-        pad[:length] = torch.tensor(indices)
-
-        return torch.tensor(pad), torch.tensor(length)
-    
+        idx = [self.bos_id] + self.indices[item][:self.max_length - 2] + [self.eos_id]
+        length = len(idx)
+        idx += [self.pad_id] * (self.max_length - length)
+        return idx, length
