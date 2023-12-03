15757f56c6cf1ce366fc0200cdcfbf3c6e537cdb
Matvey Kairov <mvkairov@edu.hse.ru>
2023-12-04 02:01:40 +0300

    model upd

--- LLAMA/model.py
+++ LLAMA/model.py
@@ -1,52 +1,65 @@
-import torch
 import torch.nn as nn
-
-def generate_square_mask(sz, device):
-    mask = (torch.triu(torch.ones((sz, sz), device=device)) == 1).transpose(0, 1)
-    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-    return mask
-
-
-def create_mask(x, pad_idx, device):
-    if len(x.shape) == 2:
-        tgt_seq_len = x.shape[1]
-    else:
-        tgt_seq_len = x.shape[0]
-    tgt_mask = generate_square_mask(tgt_seq_len, device)
-    tgt_padding_mask = (x == pad_idx)
-    return tgt_mask, tgt_padding_mask
-
+import torch
+import math
+import copy
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
+    def __init__(self, emb_size: int, maxlen: int = 5000):
+        super(PositionalEncoding, self).__init__()
+        den = torch.exp(- torch.arange(0, emb_size, 2)* math.log(10000) / emb_size)
+        pos = torch.arange(0, maxlen).reshape(maxlen, 1)
+        pos_embedding = torch.zeros((maxlen, emb_size))
+        pos_embedding[:, 0::2] = torch.sin(pos * den)
+        pos_embedding[:, 1::2] = torch.cos(pos * den)
+        pos_embedding = pos_embedding.unsqueeze(-2)
+
+        self.register_buffer('pos_embedding', pos_embedding)
+
+    def forward(self, token_embedding: torch.Tensor):
+        return token_embedding + self.pos_embedding[:token_embedding.size(0), :]
+
+class PreNormDecoderBlock(nn.Module):
+    def __init__(self, embed_dim: int, n_heads: int, hidden_dim):
         super().__init__()
-        self.dropout = nn.Dropout(p=dropout)
 
-        position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-torch.log(torch.Tensor([10000.0])).item() / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe[:x.size(0)]
-        return self.dropout(x)
-
-
-class LLAMA(nn.Module):
-    def __init__(self, n_layers, d_model, nhead, vocab_size, dim_ff, dropout, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.positional_encoding = PositionalEncoding(d_model, dropout)
-        self.embedding = nn.Embedding(vocab_size, d_model)
-        self.transformer = nn.TransformerDecoder(
-            nn.TransformerDecoderLayer(d_model, nhead, dim_ff, dropout, batch_first=True), n_layers
+        self.attention = nn.MultiheadAttention(embed_dim, n_heads, batch_first=True)
+        self.ffn = nn.Sequential(
+            nn.Linear(embed_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, embed_dim)
         )
-        self.lin = nn.Linear(d_model, vocab_size)
-        print(f'{sum([torch.prod(torch.tensor(p.shape)) for p in self.parameters() if p.requires_grad])} params :)')
+
+        self.attn_layernorm = nn.LayerNorm(embed_dim)
+        self.ffn_layernorm = nn.LayerNorm(embed_dim)
     
-    def forward(self, input_ids, attention_mask, padding_mask):
-        x = self.embedding(input_ids)
+    def forward(self, x, attention_mask, padding_mask):
+        x_norm = self.attn_layernorm(x)
+        x_attn, _ = self.attention(x_norm, x_norm, x_norm, key_padding_mask=padding_mask, attn_mask=attention_mask)
+        h = x_attn + x
+        out = h + self.ffn(self.ffn_layernorm(x))
+        return out
+
+class Decoder(nn.Module):
+    def __init__(self, decoder_layer, num_layers, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.decoder = nn.ModuleList([copy.deepcopy(decoder_layer) for i in range(num_layers)])
+    
+    def forward(self, x, attention_mask, padding_mask):
+        for layer in self.decoder:
+            x = layer(x, attention_mask, padding_mask)
+        return x
+
+class LLaMA(nn.Module):
+    def __init__(self, vocab_size, embed_dim, n_heads, hidden_dim, num_layers, max_seq_len, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.positional_encoding = PositionalEncoding(embed_dim, max_seq_len)
+        self.decoder = Decoder(PreNormDecoderBlock(embed_dim, n_heads, hidden_dim), num_layers)
+        self.linear = nn.Linear(embed_dim, vocab_size)
+    
+    def forward(self, x, attention_mask, padding_mask):
+        x = self.embedding(x)
         x = self.positional_encoding(x)
-        x = self.transformer(x, attention_mask, padding_mask)
-        return self.lin(x)
+        x = self.decoder(x, attention_mask, padding_mask)
+        x = self.linear(x)
+        return x
