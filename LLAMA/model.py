7a64ada2b98b84b122ffc1c5c6c07bd2401f3d41
Matvey Kairov <mvkairov@edu.hse.ru>
2023-12-04 02:02:41 +0300

    model fix2

--- LLAMA/model.py
+++ LLAMA/model.py
@@ -7,7 +7,7 @@ class PositionalEncoding(nn.Module):
     def __init__(self, emb_size: int, maxlen: int = 5000):
         super(PositionalEncoding, self).__init__()
         den = torch.exp(- torch.arange(0, emb_size, 2)* math.log(10000) / emb_size)
-        pos = torch.arange(0, maxlen).reshape(maxlen, 1)
+        pos = torch.arange(0, maxlen).reshape((maxlen, 1))
         pos_embedding = torch.zeros((maxlen, emb_size))
         pos_embedding[:, 0::2] = torch.sin(pos * den)
         pos_embedding[:, 1::2] = torch.cos(pos * den)
