0f70a951152a84230938c3d15612a76157d88653
Matvey Kairov <mvkairov@edu.hse.ru>
2023-12-04 06:31:21 +0300

    update

--- LLAMA/model.py
+++ LLAMA/model.py
@@ -1,108 +1,73 @@
 import torch
-from torch import Tensor
 import torch.nn as nn
-import copy
 
 
-def generate_square_mask(sz, device):
-    mask = (torch.triu(torch.ones((sz, sz), device=device)) == 1).transpose(0, 1)
+def make_seq_mask(x, pad_idx):
+    sz = x.shape[len(x.shape) == 2]
+    mask = (torch.triu(torch.ones((sz, sz), device='cuda')) == 1).transpose(0, 1)
     mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-    return mask
-
-
-def create_mask(x, pad_idx, device):
-    if len(x.shape) == 2:
-        tgt_seq_len = x.shape[1]
-    else:
-        tgt_seq_len = x.shape[0]
-    tgt_mask = generate_square_mask(tgt_seq_len, device)
-    tgt_padding_mask = (x == pad_idx)
-    return tgt_mask, tgt_padding_mask
+    padding_mask = (x == pad_idx)
+    return mask, padding_mask
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self, embed_dim, num_heads, ff_dim, dropout, batch_first=True, *args, **kwargs) -> None:
+    def __init__(self, d_model, nhead, ff_dim, dropout, batch_first=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.attention = nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout, batch_first=batch_first)
-        self.attn_dropout = nn.Dropout(dropout)
-
-        self.feed_forward = nn.Sequential(
-            nn.Linear(embed_dim, ff_dim),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(ff_dim, embed_dim),
-            nn.Dropout(dropout)
-        )
-        self.norm1 = nn.LayerNorm(embed_dim)
-        self.norm2 = nn.LayerNorm(embed_dim)
-
-    def forward(self, x, attention_mask, padding_mask):
-        x_ = self.norm1(x)
-        x_, _ = self.attention(x, x, x, attn_mask=attention_mask, key_padding_mask=padding_mask)
-        x_ = x + x_
-        x_ = self.norm2(x_)
-        x_ = x_ + self.feed_forward(x_)
-        return x_
-
-class Decoder(nn.Module):
-    def __init__(self, decoder_layer, num_layers, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.decoder = nn.ModuleList([copy.deepcopy(decoder_layer) for i in range(num_layers)])
-    
-    def forward(self, x, attention_mask, padding_mask):
-        for layer in self.decoder:
-            x = layer(x, attention_mask, padding_mask)
-        return x
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, embed_dim, dropout: float = 0.1, max_len: int = 5000):
-        """
-        Inputs
-            embed_dim - Hidden dimensionality of the input.
-            max_len - Maximum length of a sequence to expect.
-        """
-        super().__init__()
-        self.embed_dim = embed_dim
+        self.attention = nn.MultiheadAttention(d_model, nhead, dropout, batch_first=batch_first)
         self.dropout = nn.Dropout(dropout)
 
-        pe = torch.zeros(max_len, self.embed_dim)
+        self.ff = nn.Sequential(
+            nn.Linear(d_model, ff_dim),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(ff_dim, d_model),
+            nn.Dropout(dropout)
+        )
+        self.att_lnorm = nn.LayerNorm(d_model)
+        self.ff_lnorm = nn.LayerNorm(d_model)
+
+    def forward(self, x, attn_mask, pad_mask):
+        x_n = self.att_lnorm(x)
+        x_n, _ = self.attention(x, x, x, attn_mask=attn_mask, key_padding_mask=pad_mask)
+        x_n = x + self.dropout(x_n)
+        x_n += self.ff(self.ff_lnorm(x_n))
+        return x_n
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=5000):
+        super().__init__()
+        pos_enc = torch.zeros(max_len, d_model)
         pos = torch.arange(max_len).reshape(-1, 1)
-        denom = torch.pow(10000, (torch.arange(self.embed_dim) - (torch.arange(self.embed_dim) % 2)) / embed_dim)
-        pe = pos / denom
-        pe[:, 0::2] = torch.sin(pe[:, 0::2])
-        pe[:, 1::2] = torch.cos(pe[:, 1::2])
-        pe = pe.unsqueeze(0)
-        self.register_buffer('pe', pe, persistent=False)
+        den = torch.exp(-torch.arange(0, d_model, 2) * torch.log(torch.Tensor([10000])).item() / d_model)
+        # denom = torch.pow(10000, (torch.arange(self.d_model) - (torch.arange(self.d_model) % 2)) / d_model)
+        # pe = pos / den
+        pos_enc[:, 0::2] = torch.sin(pos / den)
+        pos_enc[:, 1::2] = torch.cos(pos / den)
+        pos_enc = pos_enc.unsqueeze(0)
+        self.register_buffer('pos_enc', pos_enc, persistent=False)
 
     def forward(self, x):
-        x = x + self.pe[:, :x.shape[-2], :]
-        return self.dropout(x)
+        return x + self.pos_enc[:, :x.shape[-2], :]
+
 
 class LLAMA(nn.Module):
-    def __init__(self, num_layers, embed_dim, num_heads, vocab_size, ff_dim, dropout, *args, **kwargs) -> None:
+    def __init__(self, num_layers, d_model, nhead, vocab_size, ff_dim, dropout, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.positional_encoding = PositionalEncoding(embed_dim=embed_dim, dropout=dropout)
-        self.embedding = nn.Embedding(vocab_size, embed_dim)
-        self.transformer = Decoder(
-            DecoderLayer(embed_dim, num_heads, ff_dim, dropout, batch_first=True),
+        self.positional_encoding = PositionalEncoding(d_model)
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.decoder = nn.TransformerDecoder(
+            DecoderLayer(d_model, nhead, ff_dim, dropout),
             num_layers=num_layers
         )
-        self.classification = nn.Linear(embed_dim, vocab_size)
+        self.linear = nn.Linear(d_model, vocab_size)
     
-    def __str__(self):
-        """
-        Model prints with number of trainable parameters
-        """
-        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
-        params = sum([torch.prod(torch.tensor(p.shape)) for p in model_parameters])
-        return super().__str__() + "\nTrainable parameters: {}".format(params)
-    
-    def forward(self, input_ids: Tensor, attention_mask: Tensor, padding_mask: Tensor):
+    def forward(self, input_ids, attention_mask, padding_mask):
         x = self.embedding(input_ids)
         x = self.positional_encoding(x)
-        x = self.transformer(x, attention_mask, padding_mask)
-        return self.classification(x)
+        x = self.decoder(x, attention_mask, padding_mask)
+        return self.linear(x)
     
-    def get_next_token(self, prefix: Tensor, attention_mask: Tensor, padding_mask: Tensor):
-        """ :returns: probabilities of next token """
-        return self.forward(prefix, attention_mask, padding_mask)[:, -1, :]
+    def get_next_token(self, prefix, mask, pad_mask):
+        return self.forward(prefix, mask, pad_mask)[:, -1, :]
+    
--- LLAMA/train.py
+++ LLAMA/train.py
@@ -1,20 +1,18 @@
 import torch
-from model import create_mask
+from model import make_seq_mask
 from tqdm.notebook import tqdm
 from itertools import repeat
-from torch import Tensor
 
 
 @torch.no_grad()
-def generate(model, tokenizer, batch_size, pad_idx, prefix=None, max_len=384):
+def generate(model, tokenizer, pad_idx, batch_size=1, prefix=None, max_len=384):
     model.eval()
     if prefix is None:
         prefix = torch.full((batch_size, 1), fill_value=tokenizer.bos_id()).to(next(model.parameters()).device)
-    count = max_len - prefix.shape[-1]
-    for _ in range(count):
+    for _ in range(max_len):
         prefix = prefix.clone().detach()
-        tgt_mask, tgt_padding_mask = create_mask(prefix, pad_idx, device='cuda')
-        output_logits = torch.nn.functional.softmax(model.forward(prefix, tgt_mask, tgt_padding_mask)[:, -1, :], dim=-1)
+        mask, pad_mask = make_seq_mask(prefix, pad_idx)
+        output_logits = torch.nn.functional.softmax(model.forward(prefix, mask, pad_mask)[:, -1, :], dim=-1)
         prefix = torch.cat((prefix, torch.multinomial(output_logits, 1)), dim=-1)
     return prefix
 
@@ -24,67 +22,63 @@ def inf_loop(data_loader):
         yield from loader
 
 
-def evaluate(model, dataloader, loss_fn, pad_idx, device):
+def evaluate(model, dataloader, loss_fn, pad_idx):
     model.eval()
     losses = 0
-    for tgt, _ in tqdm(dataloader):
-        tgt = tgt.to(device)
+    for tgt, _ in tqdm(dataloader, total=len(dataloader)):
+        tgt = tgt.to('cuda')
         tgt_input = tgt[:, :-1]
-        tgt_mask, tgt_padding_mask = create_mask(tgt_input, pad_idx, device)
-        with torch.autocast(device_type=device, dtype=torch.bfloat16):
-            logits = model(tgt_input, tgt_mask, tgt_padding_mask)
+        mask, pad_mask = make_seq_mask(tgt_input, pad_idx)
+        with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
+            logits = model(tgt_input, mask, pad_mask)
         tgt_out = tgt[:, 1:]
         loss = loss_fn(logits.reshape(-1, logits.shape[-1]), tgt_out.reshape(-1))
         losses += loss.item()
     return losses / len(dataloader)
 
 
-def train(n_epochs, model, pad_idx, optimizer, train_loader, val_loader, device, dataset, scheduler, wandb_instance, len_epoch, log_step):
+def train(model, n_epochs, pad_idx, optimizer, scheduler, train_loader, val_loader, dataset, wandb_instance, len_epoch, log_freq=10):
     loss_fn = torch.nn.CrossEntropyLoss(ignore_index=pad_idx)
-    train_loader_inf = inf_loop(train_loader)
-    best_loss = 1e6
+    min_loss = 100
+
     for epoch in range(n_epochs):
         losses = 0
-        for i, (tgt, _) in enumerate(tqdm(train_loader_inf, desc="train", total=len_epoch)):
+        for i, (tgt, _) in enumerate(tqdm(inf_loop(train_loader), total=len_epoch)):
             model.train()
-            tgt = tgt.to(device)
+            tgt = tgt.to('cuda')
             tgt_input = tgt[:, :-1]
-            tgt_mask, tgt_padding_mask = create_mask(tgt_input, pad_idx, device)
+            mask, padding_mask = make_seq_mask(tgt_input, pad_idx)
             optimizer.zero_grad()
-            with torch.autocast(device_type=device, dtype=torch.bfloat16):
-                logits = model(tgt_input, tgt_mask, tgt_padding_mask)
+            with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
+                logits = model(tgt_input, mask, padding_mask)
                 tgt_out = tgt[:, 1:]
                 loss = loss_fn(logits.reshape(-1, logits.shape[-1]), tgt_out.reshape(-1))
             loss.backward()
             optimizer.step()
+            scheduler.step()
             losses += loss.item()
             cur_step = epoch * len_epoch + i
 
+            if i % (len_epoch // log_freq) == 0 and i > 0:
+                wandb_instance.log({
+                    'train_loss': losses / i,
+                    'lr': scheduler.get_last_lr()[0]
+                }, step=cur_step)
+                print(f"epoch: {epoch}, train_loss: {(losses / i):.3f}")
+                losses = 0
+
             if i == len_epoch:
-                val_loss = evaluate(model, val_loader, loss_fn, pad_idx, device)
-                if val_loss < best_loss:
+                val_loss = evaluate(model, val_loader, loss_fn, pad_idx)
+                if val_loss < min_loss:
                     print(f'checkpoint at {cur_step}')
                     torch.save(model.state_dict(), 'checkpoint.pth')
-                    best_loss = val_loss
-                prefix = generate(model, dataset.sp_model, 3, pad_idx)
-                texts = dataset.ids2text(prefix)
-                for t_num, text in enumerate(texts):
-                    wandb_instance.log({
-                        f'stepN{cur_step}_textN{t_num}': wandb_instance.Html(text)
-                    }, step=cur_step)
+                    min_loss = val_loss
+                text = dataset.ids2text(generate(model, dataset.sp_model, pad_idx))
+                wandb_instance.log({f'stepN{cur_step}': text}, step=cur_step)
                 wandb_instance.log({
-                    'train_loss': losses / ((epoch * len_epoch + 1) + i),
+                    'train_loss': losses / len_epoch,
                     'val_loss': val_loss,
                     'lr': scheduler.get_last_lr()[0]
                 }, step=cur_step)
-                print(f"epoch: {epoch}, train loss: {(losses / (epoch * len_epoch + 1) + i):.3f}, val loss: {val_loss:.3f}")
+                print(f'epoch: {epoch}, train_loss: {(losses / i):.3f}, val_loss: {val_loss:.3f}')
                 break
-
-            if i % log_step == 0 and i > 0 or i == 1:
-                wandb_instance.log({
-                    'train_loss': losses / log_step,
-                    'lr': scheduler.get_last_lr()[0]
-                }, step=cur_step)
-                print(f"epoch: {epoch}, train loss: {(losses / log_step):.3f}")
-                losses = 0
-            scheduler.step()
